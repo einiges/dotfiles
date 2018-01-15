d6b02331fc7d445927327ca892172a58ed3fed21
Jens Henniges <jens.henniges@gmx.net>
2018-01-15 12:27:41 +0100

    fix: move variable settings to post

--- zsh/zshrc.d/plugin-hooks/zsh-history-substring-search.post.zsh
+++ zsh/zshrc.d/plugin-hooks/zsh-history-substring-search.post.zsh
@@ -1,2 +1,5 @@
+HISTORY_SUBSTRING_SEARCH_FUZZY=1
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND=bg=magenta,fg=black,bold
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND=bg=red,fg=black,bold
 bindkey '^n' history-substring-search-up
 bindkey '^r' history-substring-search-down

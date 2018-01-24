2e780906fbc8fa8400b193439eb1e5abd6d3636a
Jens Henniges <jens.henniges@gmx.net>
2018-01-24 22:03:06 +0100

    feature: apply 'fix keyboard input'-patch to st

--- zsh/zshrc.d/80-keybindings.zsh
+++ zsh/zshrc.d/80-keybindings.zsh
@@ -1,20 +1,43 @@
 #!/bin/zsh
 
+#autoload -Uz select-word-style
+#select-word-style bash
+WORDCHARS="?_-.~;!#$%^"
+
+autoload backward-kill-word-match
+zle -N backward-kill-word-match
+
+autoload kill-word-match
+zle -N kill-word-match
+
 # Vim Mode
 bindkey -v
 
-bindkey "^[[4~"    end-of-line           # End
-bindkey "^[[5~"    backward-word         # PageUp/Prior
-bindkey "^[[6~"    forward-word          # PageDown/Next
 
-case $TERM in
-	tmux* )
-		bindkey "^[[1~"    beginning-of-line     # Home
-		bindkey "^[[3~"    delete-char           # Delete
-		;;
-	st* )
-		bindkey "^[[H"     beginning-of-line     # Home
-		bindkey "^[[P"     delete-char           # Delete
-		;;
-esac
+
+helper=${ZDOTDIR}/zshrc.d/keys/${TERM}
+if [[ $TERM =~ '^tmux-.*' ]]
+then helper+="_${$(tmux show-environment -g TERM)#*=}"
+fi
+
+if [[ -f $helper ]]; then
+	source $helper
+	unset helper
+else
+	return
+fi
+
+[[ -n ${key[Backspace]} ]]    && bindkey "${key[Backspace]}" backward-delete-char
+[[ -n ${key[C-Backspace]} ]]  && bindkey "${key[C-Backspace]}" backward-kill-word-match
+#[[ -n ${key[Insert]} ]]       && bindkey "${key[Insert]}" overwrite-mode
+[[ -n ${key[Home]} ]]         && bindkey "${key[Home]}" beginning-of-line
+[[ -n ${key[PageUp]} ]]       && bindkey "${key[PageUp]}" backward-word
+[[ -n ${key[Delete]} ]]       && bindkey "${key[Delete]}" delete-char
+[[ -n ${key[C-Delete]} ]]     && bindkey "${key[C-Delete]}" kill-word-match
+[[ -n ${key[End]} ]]          && bindkey "${key[End]}" end-of-line
+[[ -n ${key[PageDown]} ]]     && bindkey "${key[PageDown]}" forward-word
+[[ -n ${key[Up]} ]]           && bindkey "${key[Up]}" up-line-or-search
+[[ -n ${key[Left]} ]]         && bindkey "${key[Left]}" backward-char
+[[ -n ${key[Down]} ]]         && bindkey "${key[Down]}" down-line-or-search
+[[ -n ${key[Right]} ]]        && bindkey "${key[Right]}" forward-char
 

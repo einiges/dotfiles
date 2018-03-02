e7bec844e894e89a0a9ade77b1962881a0da969d
Jens Henniges <jens.henniges@gmx.net>
2018-03-02 11:08:44 +0100

    fix: missing files

---
+++ zsh/hooks/title.zsh
@@ -0,0 +1,14 @@
+
+# Set window title, when waiting for command
+function title_idle {
+	print -Pn "\e]0;%(1j,%j job%(2j|s|); ,)%~\a"
+}
+
+# Set title when executing a command
+function title_work {
+	printf "\033]0;%s\a" "$1"
+}
+
+add-zsh-hook precmd  title_idle
+add-zsh-hook preexec title_work
+

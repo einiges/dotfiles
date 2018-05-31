4b3c6cc42b1e52266171202c1a7c36eb27b507d6
Jens Henniges <jens.henniges@gmx.net>
2018-05-31 14:58:28 +0200

    change: more comfortable dirhashes

--- zsh/dirhashes.zsh
+++ zsh/dirhashes.zsh
@@ -1,19 +1,14 @@
 #!/bin/zsh
 
-hash -d Load=~/Downloads
-hash -d Doc=~/Documents
-hash -d Pic=~/Pictures
-hash -d Vid=~/Videos
-hash -d Music=~/Audio/Music
-hash -d Radioplay=~/Audio/RadioPlay
-hash -d Wall=~/Pictures/Wallpapers
-hash -d Screenshots=~/Pictures/Screenshots
+hash -d load=~/Downloads
+hash -d doc=~/Documents
+hash -d music=~/Audio/Music
+hash -d radio=~/Audio/RadioPlay
+hash -d wall=~/Pictures/Wallpapers
+hash -d screenshots=~/Pictures/Screenshots
 
-hash -d  zsh="$XDG_CONFIG_HOME"/zsh
 hash -d nvim="$XDG_CONFIG_HOME"/nvim
-hash -d bsp="$XDG_CONFIG_HOME"/bspwm
-hash -d polybar="$XDG_CONFIG_HOME"/polybar
 
-hash -d publ=~/prjects/public
-hash -d priv=~/projects/private
-hash -d silly=~/projects/silly
+hash -d pub=~/prjects/public
+hash -d pri=~/projects/private
+hash -d sil=~/projects/silly

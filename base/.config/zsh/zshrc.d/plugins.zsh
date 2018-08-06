d48d65bdfbfaaed17c95f1328559bf7ca4900287
Jens Henniges <jens.henniges@gmx.net>
2018-08-06 12:37:31 +0200

    change: skip init script existence check

--- base/.config/zsh/zshrc.d/plugins.zsh
+++ base/.config/zsh/zshrc.d/plugins.zsh
@@ -3,6 +3,9 @@
 ZGEN_DIR="${XDG_CACHE_HOME}/zsh/zgen"
 ZGEN_INIT="${XDG_CACHE_HOME}/zsh/zgen-plugins-loader.zsh"
 
+# zgen completion
+fpath+=($ZGEN_DIR)
+
 zgen () {
 	if [[ ! -s $ZGEN_DIR/zgen.zsh ]]; then
 		mkdir -p ${ZGEN_DIR:h}
@@ -13,31 +16,28 @@ zgen () {
 }
 
 
-# Generate zgen init script if needed
-if [[ ! -s "$ZGEN_INIT" ]]; then
-	[[ -f ${ZDOTDIR}/.zcompdump ]] && rm -f ${ZDOTDIR}/.zcompdump
-
-	zgen load "zsh-users/zsh-history-substring-search"
-	zgen load "zsh-users/zsh-autosuggestions"
-	zgen load "zdharma/fast-syntax-highlighting"
-	zgen load "rupa/z"
-
-	zgen oh-my-zsh "plugins/fancy-ctrl-z"
-	zgen oh-my-zsh "plugins/extract"
-
-	zgen load "mafredri/zsh-async"
-	zgen load "$HOME/Projects/public/zsh-bdi"
-
-	zgen save
-fi
-
 for f in ${ZDOTDIR}/plugin-config/*.pre.zsh; do
 	source $f
 done
 
+# Generate zgen init script if needed
 # Static load plugins
-. ${ZGEN_INIT}
+source ${ZGEN_INIT} 2>/dev/null || {
+	[[ -f ${ZDOTDIR}/.zcompdump ]] && rm -f ${ZDOTDIR}/.zcompdump
 
+	zgen load zsh-users/zsh-history-substring-search
+	zgen load zsh-users/zsh-autosuggestions
+	zgen load zdharma/fast-syntax-highlighting
+	zgen load rupa/z
+
+	zgen oh-my-zsh plugins/fancy-ctrl-z
+	zgen oh-my-zsh plugins/extract
+
+	zgen load mafredri/zsh-async
+	zgen load "$HOME/Projects/public/zsh-bdi"
+
+	zgen save
+}
 
 for f in ${ZDOTDIR}/plugin-config/*.post.zsh; do
 	source $f

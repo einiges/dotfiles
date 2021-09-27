c5b5d9f6543bf1f435439c2c918fde77967e4299
Jens Henniges <>
2021-09-27 14:57:01 +0200

    change: shorter hashdirs

--- zsh/.config/zsh/zshrc.d/dirhashes.zsh
+++ zsh/.config/zsh/zshrc.d/dirhashes.zsh
@@ -1,9 +1,9 @@
-hash -d load=~/Downloads
+hash -d dl=~/Downloads
 hash -d doc=~/Documents
 hash -d music=~/Audio/Music
 hash -d radio=~/Audio/RadioPlay
 hash -d wall=~/Pictures/Wallpapers
-hash -d screenshots=~/Pictures/Screenshots
+hash -d shots=~/Pictures/Screenshots
 hash -d media=/run/media/$USER
 
 hash -d nvim="$XDG_CONFIG_HOME"/nvim

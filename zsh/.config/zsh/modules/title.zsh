208ba85f86dc689835a12253dc57b82c2e4ee7be
Jens Henniges <>
2021-10-03 13:38:16 +0200

    fix: issue that output was printed twice when using ANSI escape codes.

--- zsh/.config/zsh/modules/title.zsh
+++ zsh/.config/zsh/modules/title.zsh
@@ -5,19 +5,17 @@ case $TTY in
 	/dev/ttyS[0-9]*) return ;;
 esac
 
-
-autoload -Uz add-zsh-hook
-
 # Set window title, when waiting for command
-function title_idle {
-	print -Pn "\e]0;%(1j,%j job%(2j|s|); ,)%~\a"
+function __title::idle {
+	print -Pn -- '\e]0;%(1j,%j job%(2j|s|); ,)%~\a'
 }
 
 # Set title when executing a command
-function title_work {
-	print -n "\e]0;$1\a"
+function __title::work {
+	print -Pn -- '\e]0;$2\a'
 }
 
-add-zsh-hook precmd  title_idle
-add-zsh-hook preexec title_work
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd  __title::idle
+add-zsh-hook preexec __title::work
 

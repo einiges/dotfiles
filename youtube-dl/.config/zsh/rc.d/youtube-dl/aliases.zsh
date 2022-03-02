bda03fc14c89943e8ee71b0f73f034a9c1d082c8
Jens Henniges <>
2022-03-02 21:06:14 +0100

    add youtube-dl config and use zsh config dir structure with it

---
+++ youtube-dl/.config/zsh/rc.d/youtube-dl/aliases.zsh
@@ -0,0 +1,5 @@
+alias ytdl='youtube-dl'
+#alias ytdll='youtube-dl --batch-file ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/todo}'
+#alias ytdla='youtube-dl --download-archive ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/archive}'
+#alias ytdlla='youtube-dl --batch-file ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/todo} --download-archive ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/archive}'
+
--- zsh/.config/zsh/rc.d/aliases.zsh
+++ zsh/.config/zsh/rc.d/aliases.zsh
@@ -48,9 +48,3 @@ alias rs='rsync -ahu'
 alias TS='print -P -f "%s" "%D{%Y%m%d%H%M%S}"'
 alias TSH='print -P -f "%s" "%D{%Y-%m-%d %H:%M:%S}"'
 
-alias ytdl='youtube-dl'
-alias ytdll='youtube-dl --batch-file ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/todo}'
-alias ytdla='youtube-dl --download-archive ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/archive}'
-alias ytdlla='youtube-dl --batch-file ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/todo} --download-archive ${YOUTUBE_DL_BATCH_FILE:-${XDG_DATA_HOME:-$HOME/.local/share}/youtube-dl/archive}'
-
-

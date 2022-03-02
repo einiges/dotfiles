96431a830b7dc2dab556ec5bd59bcacdba61020c
Jens Henniges <>
2022-03-02 20:18:46 +0100

    revamp X11 dir

--- X11/.config/X11/xinitrc.d/80-keyboard.sh
+++ X11/.config/X11/xinitrc.d/80-keyboard.sh
@@ -4,5 +4,6 @@ setxkbmap -layout de -variant neo -option altwin:swap_lalt_lwin
 # Alter existing layout
 #xmodmap $XDG_CONFIG_HOME/X11/xmodmap
 
-xcape -e 'ISO_Level3_Shift=Escape'
+command -v xcape >/dev/null 2>&1 &&
+	xcape -e 'ISO_Level3_Shift=Escape'
 
---
+++ X11/.config/zsh/env.d/X11/env.zsh
@@ -0,0 +1,8 @@
+export    XINITRC="$XDG_CONFIG_HOME"/X11/xinitrc
+export  XSERVERRC="$XDG_CONFIG_HOME"/X11/xserverrc
+export XAUTHORITY="$XDG_RUNTIME_DIR"/xauthority
+export USERXSESSION="$XDG_CONFIG_HOME"/X11/xsession
+export USERXSESSIONRC="$XDG_CACHE_HOME"/X11/xsessionrc
+export ALTUSERXSESSION="$XDG_CACHE_HOME"/X11/Xsession
+export ERRFILE="$XDG_CACHE_HOME"/X11/xsession-errors
+

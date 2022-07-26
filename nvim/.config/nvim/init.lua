bea70b73274277d97df4609eb6cbc1e7227b34f0
Jens Henniges <>
2022-07-26 17:14:56 +0200

    lua filetype detection is default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,9 +3,6 @@ if pcall(require, 'impatient') then
 	require('impatient').enable_profile()
 end
 
-vim.g.do_filetype_lua = 1    -- enable  lua filetype detection
-vim.g.did_load_filetypes = 0 -- disable vim filetype detection
-
 require('my.general.disable_builtin')
 
 require('my.general.options')
